9763ea13271caa30302ec61eb4a30e7d1a7e1424
cfhj8520 <cfhj8520@naver.com>
2021-09-10 15:10:41 +0900

    이자 계산 조건문 수정

--- BankingSystem/src/bank/BasicInterestCalculator.java
+++ BankingSystem/src/bank/BasicInterestCalculator.java
@@ -9,11 +9,11 @@ public class BasicInterestCalculator implements InterestCalculator{
         // TODO:
         //  예금 계좌의 경우 잔액이 1000만원 이상은 이자율이 50%,
         //  500만원 이상은 7%, 100만원 이상은 4%, 1만원 이상은 2%, 그 외에는 1% 입니다.
-        if(balance.compareTo(new BigDecimal("5000000")) > 0){
+        if(balance.compareTo(new BigDecimal("5000000")) >= 0){
             interest = balance.multiply(new BigDecimal("0.07"));
-        }else if(balance.compareTo(new BigDecimal("1000000")) > 0){
+        }else if(balance.compareTo(new BigDecimal("1000000")) >= 0){
             interest = balance.multiply(new BigDecimal("0.04"));
-        }else if(balance.compareTo(new BigDecimal("10000")) > 0){
+        }else if(balance.compareTo(new BigDecimal("10000")) >= 0){
             interest = balance.multiply(new BigDecimal("0.02"));
         }else{
             interest = balance.multiply(new BigDecimal("0.01"));
